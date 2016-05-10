052b6ee98d1798f0533e8aed8c0e30a019e31b57
Camilo Crespo <crezam@live.com>
2016-05-09 23:51:31 -0400

    Access var from handler

--- gorilla/gorilla-mux-vars.go
+++ gorilla/gorilla-mux-vars.go
@@ -1,1 +1,17 @@
-package gorilla
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func AcctHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.Write([]byte(vars["id"]))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/account/{id}", AcctHandler)
+	http.ListenAndServe(":8080", r)
+}
