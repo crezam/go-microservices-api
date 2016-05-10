a71b383c9f647cfdd3529bdf8d80dba662b39a38
Camilo Crespo <crezam@live.com>
2016-05-09 23:39:56 -0400

    Simple handler

--- gorilla/gorilla-mux-simple.go
+++ gorilla/gorilla-mux-simple.go
@@ -1,1 +1,17 @@
-package gorilla
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func SimpleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello\n"))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", SimpleHandler)
+	// Bind to a port and pass our router in
+	http.ListenAndServe(":8080", r)
+}
