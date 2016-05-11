42c05108a31dad245f0ee9f23c5ef861ec524d17
Camilo Crespo <crezam@live.com>
2016-05-10 23:48:52 -0400

    Basic mux

--- negroni/negroni-middleware-simple.go
+++ negroni/negroni-middleware-simple.go
@@ -1,1 +1,13 @@
-package negroni
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello")
+	})
+}
